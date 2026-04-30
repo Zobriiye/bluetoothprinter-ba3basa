2462cba670e30b9347b43dd300482891ad99b095
Zobriiye <144528605+Zobriiye@users.noreply.github.com>
2026-04-30 16:45:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const main = () => {
     const parameters = new URLSearchParams(window.location.search);
     let params = {};
     const cleanUpData = (key) => {
-        return key.split(",").map((ind) => ind.trim());
+        if (!params[key]) return [];
+        return params[key].split(",").map((ind) => ind.trim());
     };
     for (var value of parameters.keys()) {
         params[value] = parameters.get(value);
@@ -12,88 +13,106 @@ const main = () => {
     const {
         products,
         quantity,
-        unitpriceusd,
+        unitpriceusd,    // actual unit price
+        subtotalusd,     // actual subtotal
         customer,
         location,
         date, companyName, phoneNumber,
         totalLbp, target,
-        totalusd, pagewidth, savePdf, splitSubTotal, potsRemainingLarge, transId,
+        totalusd, pagewidth, savePdf, potsRemainingLarge, transId,
         potsRemainingMedium, enablePots
     } = params;
 
     const prodArr = [];
-    cleanUpData(products).forEach((name) => prodArr.push({ name }));
-    const quantities = cleanUpData(quantity);
-    quantities.forEach((quantity, i) => (prodArr[i].quantity = quantity));
+    const productNames = cleanUpData("products");
+    const quantities = cleanUpData("quantity");
+    const unitPrices = cleanUpData("unitpriceusd");
+    const subtotals = cleanUpData("subtotalusd");
 
-    cleanUpData(unitpriceusd).forEach((usdVal, i) => {
-        if (!splitSubTotal) {
-            prodArr[i].unitpriceusd = usdVal;
-            prodArr[i].subtotalusd = usdVal;
-        } else {
-            const subtotal = parseFloat(usdVal);
-            const qty = parseInt(quantities[i]);
-            prodArr[i].unitpriceusd = subtotal !== 0 ? (subtotal / qty).toFixed(2) : 0;
-            prodArr[i].subtotalusd = subtotal !== 0 ? subtotal.toFixed(2) : 0;
-        }
+    productNames.forEach((name, i) => {
+        prodArr.push({
+            name: name,
+            quantity: quantities[i] || "",
+            unitpriceusd: unitPrices[i] || "0",
+            subtotalusd: subtotals[i] || "0"
+        });
     });
 
+    // Pots section
     const potsRemainingLargePart = document.querySelector("#potsRemainingLarge");
     const potsRemainingMediumPart = document.querySelector("#potsRemainingMedium");
     const totalpricelbpPart = document.querySelector("#totalpricelbp");
     const totalpriceusdPart = document.querySelector("#totalpriceusd");
     const potsSection = document.querySelector("#potsSection");
 
-    potsRemainingLargePart.innerHTML = (potsRemainingLarge ? potsRemainingLarge : 0) + ' L';
-    potsRemainingMediumPart.innerHTML = (potsRemainingMedium ? potsRemainingMedium : 0) + ' M';
+    if (potsRemainingLargePart) potsRemainingLargePart.innerHTML = (potsRemainingLarge ? potsRemainingLarge : 0) + ' L';
+    if (potsRemainingMediumPart) potsRemainingMediumPart.innerHTML = (potsRemainingMedium ? potsRemainingMedium : 0) + ' M';
 
     if (enablePots === undefined || enablePots === "") {
-        potsSection.remove();
+        if (potsSection) potsSection.remove();
     }
 
-    totalpriceusdPart.innerHTML = totalusd;
-    totalpricelbpPart.innerHTML = totalLbp;
+    // Totals
+    if (totalpriceusdPart) totalpriceusdPart.innerHTML = totalusd || "0";
+    if (totalpricelbpPart) totalpricelbpPart.innerHTML = totalLbp || "0";
 
+    // Company name
     const companyNamePart = document.querySelector("#companyName");
     if (!companyName) {
-        companyNamePart.remove();
+        if (companyNamePart) companyNamePart.remove();
     } else {
-        companyNamePart.innerHTML = companyName + "       INV#" + transId;
+        if (companyNamePart) companyNamePart.innerHTML = companyName + "       INV#" + transId;
     }
 
+    // Phone
     const phoneNumberPart = document.querySelector("#phoneNumber");
     if (!phoneNumber) {
-        phoneNumberPart.remove();
+        if (phoneNumberPart) phoneNumberPart.remove();
     } else {
-        phoneNumberPart.innerHTML = phoneNumber;
+        if (phoneNumberPart) phoneNumberPart.innerHTML = phoneNumber;
     }
 
-    document.querySelector("#customer").innerHTML = customer + ", " + location;
+    // Customer & location
+    const customerPart = document.querySelector("#customer");
+    if (customerPart) customerPart.innerHTML = customer + ", " + location;
 
+    // Date
     const datePart = document.querySelector("#date");
-    datePart.innerHTML = date;
+    if (datePart) datePart.innerHTML = date;
 
+    // Build table rows
     const tableBody = document.querySelector("#tableBod");
-    prodArr.forEach((product) => {
-        tableBody.innerHTML += '<tr>' +
-            '<td class="quantity">' + product.quantity + '</td>' +
-            '<td class="description">' + product.name + '</td>' +
-            '<td class="price">' + product.unitpriceusd + '</td>' +
-            '<td class="price">' + product.subtotalusd + '</td>' +
-            '</tr>';
-    });
+    if (tableBody) {
+        tableBody.innerHTML = "";
+        prodArr.forEach((product) => {
+            tableBody.innerHTML += `
+                <tr>
+                    <td class="quantity">${product.quantity}</td>
+                    <td class="description">${product.name}</td>
+                    <td class="price">${product.unitpriceusd}</td>
+                    <td class="price">${product.subtotalusd}</td>
+                </tr>
+            `;
+        });
+    }
 
+    // Print / PDF
     const ticket = document.querySelector("#ticket");
-    ticket.style.maxWidth = pagewidth;
-    ticket.style.width = pagewidth;
-    var doc = new jsPDF(ticket.clientHeight < ticket.clientWidth ? "l" : "p", "px", [ticket.clientHeight, ticket.clientWidth]);
-    doc.html(ticket, {
-        callback: function (doc) {
-            const base64Full = doc.output('datauri');
-            if (savePdf) doc.save();
-            document.location.href =
-                "rawbt:data:application/pdf;base64," + base64Full.split("base64,")[1];
-        }
-    });
+    if (ticket) {
+        ticket.style.maxWidth = pagewidth;
+        ticket.style.width = pagewidth;
+        var doc = new jsPDF(
+            ticket.clientHeight < ticket.clientWidth ? "l" : "p",
+            "px",
+            [ticket.clientHeight, ticket.clientWidth]
+        );
+        doc.html(ticket, {
+            callback: function (doc) {
+                const base64Full = doc.output('datauri');
+                if (savePdf) doc.save();
+                document.location.href = "rawbt:data:application/pdf;base64," + base64Full.split("base64,")[1];
+            }
+        });
+    }
 };
 main();
